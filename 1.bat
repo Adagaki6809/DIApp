303c2cf0945493a97ec9b97f7078dad2f4755b0f
Adagaki6809 <67869992+Adagaki6809@users.noreply.github.com>
2021-04-27 17:24:03 +0300

    Delete 1.bat

--- 1.bat
+++
@@ -1,6 +0,0 @@
-(for %%I in (.) do set CurrDirName=%%~nxI)
-copy "D:\Programs\asp.net core .gitignore\.gitignore" .gitignore
-git init && git add --all && git commit -m "first commit" && git remote add origin "https://github.com/Adagaki6809/%CurrDirName%.git" && git push origin master
-
-
-PAUSE
